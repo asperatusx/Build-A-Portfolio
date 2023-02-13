47d8b70176d479ae8ba489b96642dfd4e76fe641
Andrew Lang <112670330+asperatusx@users.noreply.github.com>
2023-02-12 22:20:35 -0500

    add img and styles for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Andrew Lang</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './Home.scss'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Header />
   )
 }
 
